855982c3376aa6c8c03cd03d447a753ba6e69cc7
kamalpandey27 <kamal0000027@gmail.com>
2026-01-12 19:04:11 +0530

    hero and navbar done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { assets } from "../assets/assets";
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [open]);
+
   return (
     <>
-      <header className="w-screen">
-        <nav>
+      <header className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all bg-light">
+        <nav className="flex items-center justify-between w-full">
           <div>
             <Link to="/">
               <img src={assets.logo} alt="logo" />
             </Link>
           </div>
-          <ul>
+
+          {/* for laptop and tablet */}
+
+          <ul className=" hidden md:flex items-center justify-between xl:gap-7 md:gap-5  text-[16px]">
             <li>
               <NavLink to="/">Home</NavLink>
             </li>
@@ -21,17 +43,83 @@ function Navbar() {
             <li>
               <NavLink to="/mybookings">My Bookings</NavLink>
             </li>
-            <li>
-              <input type="text" placeholder="Search cars" />
+            <li className="hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56">
+              <input
+                type="text"
+                placeholder="Search cars"
+                className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+              />
+              <img src={assets.search_icon} alt="search" />
             </li>
             <li>
               <NavLink to="/dashboard">Dashboard</NavLink>
             </li>
             <li>
               <NavLink to="/signup">
-                <button type="button">SignUp</button>
+                <button
+                  type="button"
+                  className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg"
+                >
+                  SignUp
+                </button>
               </NavLink>
             </li>
+          </ul>
+
+          <button
+            onClick={() => setOpen((prev) => !prev)}
+            className="md:hidden flex cursor-pointer"
+          >
+            <img src={assets.menu_icon} alt="menu_icon" />
+          </button>
+
+          {/* for mobiles */}
+
+          <ul
+            ref={menuRef}
+            className={`bg-white md:hidden absolute top-0 right-0 h-screen w-[60vw] flex flex-col  xl:gap-7 md:gap-5 gap-3  text-[16px] p-5 transition-all duration-300 ease-in-out ${
+              open
+                ? "translate-x-0 opacity-100 pointer-events-auto "
+                : "translate-x-full opacity-0 pointer-events-none"
+            }`}
+          >
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cars">Cars</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mybookings">My Bookings</NavLink>
+            </li>
+            <li className="hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56">
+              <input
+                type="text"
+                placeholder="Search cars"
+                className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+              />
+              <img src={assets.search_icon} alt="search" />
+            </li>
+            <li>
+              <NavLink to="/dashboard">Dashboard</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup">
+                <button
+                  type="button"
+                  className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg"
+                >
+                  SignUp
+                </button>
+              </NavLink>
+            </li>
+
+            <button
+              onClick={() => setOpen((prev) => !prev)}
+              className="h-5 absolute top-5 right-8"
+            >
+              <img src={assets.close_icon} alt="close_icon" />
+            </button>
           </ul>
         </nav>
       </header>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,67 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 function Home() {
-  return <>Home</>;
+  const location = ["Pickup Location", "Panipat", "Samalkha", "Delhi"];
+  const [selectLocation, setSelectLocation] = useState(
+    "Please select location"
+  );
+  return (
+    <>
+      <main className="bg-light overflow-x-hidden">
+        <section className="sm:h-screen h-[85vh] w-screen flex sm:justify-center justify-around sm:m-auto my-3 gap-5 items-center flex-col ">
+          <div className="text-4xl md:text-5xl font-semibold text-center">
+            Luxury cars on Rent
+          </div>
+          <form
+            action=""
+            className=" xl:w-[60vw] lg:w-[75vw] w-[90vw] md:h-auto sm:h-[40vh]  py-8 sm:rounded-full rounded flex justify-center items-center flex-col md:flex-row lg:gap-20 sm:gap-10 gap-5 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
+          >
+            <div className="flex lg:gap-10 gap-5  items-center sm:flex-row flex-col justify-center ">
+              <div className="flex flex-col justify-center items-center gap-3 lg:ml-10">
+                <select
+                  name=""
+                  id=""
+                  onClick={(e) => setSelectLocation(e.target.value)}
+                >
+                  {location.map((item, index) => {
+                    return (
+                      <option value={item} key={index}>
+                        {item}
+                      </option>
+                    );
+                  })}
+                </select>
+                <span className="text-gray-400 text-sm">
+                  {" "}
+                  {selectLocation || "Please select location"}
+                </span>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-3">
+                <label for="pickupdate">Pick-up Date</label>
+                <input type="date" id="pickupdate" className="text-gray-400" />
+              </div>
+              <div className="flex flex-col gap-3 justify-center items-center">
+                <label for="returndate">Return Date</label>
+                <input type="date" id="returndate" className="text-gray-400" />
+              </div>
+            </div>
+            <button
+              className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
+              type="submit"
+            >
+              <img
+                src={assets.search_icon}
+                alt="search"
+                className="brightness-300"
+              />
+              Search
+            </button>
+          </form>
+          <img src={assets.main_car} alt="main_car" className="max-h-74" />
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Home;
