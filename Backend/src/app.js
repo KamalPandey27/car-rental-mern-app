c2b4e9903fa441390acc43267e83a9ac784df679
kamalpandey27 <kamal0000027@gmail.com>
2026-02-03 18:18:24 +0530

    feat: add root route handler to provide backend status message

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,6 +9,12 @@ app.use(
     credentials: true,
   }),
 );
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Car Rental Backend is running 🚀",
+  });
+});
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
